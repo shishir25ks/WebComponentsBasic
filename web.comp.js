ef263b2baac367921f3ef9f2c5173e6204eec2de
Singh <e093886@gh-jtv8mq2.corp.mastercard.org>
2020-04-08 10:13:57 +0530

    adding callbacks

--- web.comp.js
+++ web.comp.js
@@ -1,7 +1,21 @@
 class AppDrawer extends HTMLElement {
+
     constructor() {
         super();
         console.log('in constructor');
+    }
+
+    // called every time when element is inserted in DOM
+    connectedCallback() {
+        console.log('connected!');
+    }
+    // called every time when element is removed from DOM
+    disconnectedCallback() {
+        console.log('disconnected!');
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.log(`Attribute: ${name} changed!`);
     }
 }
 
